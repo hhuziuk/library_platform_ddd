921ba33205e9cf28925695af87540baa0ca944fa
hhuziuk <huziukwork@gmail.com>
2023-09-19 18:09:56 +0200

    added TypeSchema to MongoDB

---
+++ infrastructure/db/MongoModels/TypeSchema.ts
@@ -0,0 +1,33 @@
+import mongoose, {Schema} from 'mongoose';
+
+const TypeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 1,
+        maxlength: 50,
+        validate: {
+            validator: (value) => {
+                return value.length <= 50;
+            },
+            message: 'Name is too long',
+        },
+    },
+    books: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Book',
+        },
+    ],
+    publishers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Publisher',
+        },
+    ],
+});
+
+const Type = mongoose.model('Type', TypeSchema);
+
+export default Type;
