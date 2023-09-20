a51cdd7f8eb13a76bbdc31937ee6117454b740bf
hhuziuk <huziukwork@gmail.com>
2023-09-20 16:05:20 +0200

    rewrited WishListBooksModel on MicroORM

--- infrastructure/db/entities/PostgresEntities/WishListBooksModel.ts
+++ infrastructure/db/entities/PostgresEntities/WishListBooksModel.ts
@@ -1,14 +1,14 @@
-import {Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, OneToMany} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import {Wishlist} from "./WishListModel";
 import {Book} from "./BookModel";
+
 
 @Entity('WishlistBook')
 export class WishlistBook {
     @PrimaryGeneratedColumn()
     public id: number;
 
-
-    @ManyToOne(() => Wishlist, basket => basket.wishlistBooks)
+    @ManyToOne(() => Wishlist, wishlist => wishlist.wishlistBooks)
     wishlist: Wishlist;
 
     @ManyToOne(() => Book, book => book.wishlistBooks)
