d9e020af8453a44cfaa4e290228ace79a6f15f3c
hhuziuk <huziukwork@gmail.com>
2023-10-16 20:48:48 +0200

    deleted unused libraries in index.ts in  GraphQLSchemas folder

--- infrastructure/services/GraphQLInfrastructure/GraphQLSchemas/index.ts
+++ infrastructure/services/GraphQLInfrastructure/GraphQLSchemas/index.ts
@@ -1,22 +1,9 @@
-import {GraphQLID, GraphQLList, GraphQLNonNull, GraphQLObjectType} from "graphql/type";
-import {BookType} from "./TypeDefs/BookTypeDef";
+import {GraphQLObjectType} from "graphql/type";
 import {bookResolvers} from "./resolvers/BookResolver";
-import {GraphQLSchema, GraphQLString} from "graphql";
-import {UserType} from "./TypeDefs/UserTypeDef";
+import {GraphQLSchema} from "graphql";
 import {userResolvers} from "./resolvers/UserResolver";
-import {User} from "../../../db/entities/PostgresEntities/UserModel";
-import PostgresUserRepository from "../../../db/repositories/PostgresRepository/PostgresUserRepository";
 import {typeResolvers} from "./resolvers/TypeResolver";
-import {TypeType} from "./TypeDefs/TypeTypeDef";
 import {publisherResolvers} from "./resolvers/PublisherResolver";
-import {PublisherType} from "./TypeDefs/PublisherTypeDef";
-import PostgresBookRepository from "../../../db/repositories/PostgresRepository/PostgresBookRepository";
-import {Book} from "../../../db/entities/PostgresEntities/BookModel";
-import {Publisher} from "../../../db/entities/PostgresEntities/PublisherModel";
-import PostgresPublisherRepository from "../../../db/repositories/PostgresRepository/PostgresPublisherRepository";
-import PostgresTypeRepository from "../../../db/repositories/PostgresRepository/PostgresTypeRepository";
-import {Type} from "../../../db/entities/PostgresEntities/TypeModel";
-import {ApolloServer} from "@apollo/server/dist/cjs";
 
 
 const RootQuery = new GraphQLObjectType({
