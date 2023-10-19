69bd963d51df410bb8226ca28086c8d56473464f
hhuziuk <huziukwork@gmail.com>
2023-10-19 19:54:44 +0200

    changed types for id fields in core/domain/Book.ts

--- core/domain/Book.ts
+++ core/domain/Book.ts
@@ -1,12 +1,12 @@
 export class Book {
     constructor(
-        readonly id: any,
+        readonly id: string | number,
         readonly name: string,
         readonly author: string,
         readonly description: string,
         readonly file: string,
         readonly ISBN: string,
-        readonly typeId: any,
-        readonly publisherId: any
+        readonly typeId: string | number,
+        readonly publisherId: string | number
     ) {}
 }
