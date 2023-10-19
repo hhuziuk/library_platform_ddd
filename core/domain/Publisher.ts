38611550daa5544e34cf588f210e73834324914a
hhuziuk <huziukwork@gmail.com>
2023-10-19 20:05:44 +0200

    changed types for id fields in core/domain/Publisher.ts

--- core/domain/Publisher.ts
+++ core/domain/Publisher.ts
@@ -2,7 +2,7 @@ import {Book} from "./Book";
 import {Type} from "./Type";
 export class Publisher {
     constructor(
-        readonly id: any,
+        readonly id: string | number,
         readonly name: string,
         readonly books: Book[],
         readonly types: Type[]
