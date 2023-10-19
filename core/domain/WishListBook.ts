2cf2b0f622266eb47a2e5d34f57a768faad976d7
hhuziuk <huziukwork@gmail.com>
2023-10-19 20:25:27 +0200

    changed types for id fields in core/domain/WishListBook.ts

--- core/domain/WishListBook.ts
+++ core/domain/WishListBook.ts
@@ -3,7 +3,7 @@ import {WishList} from "./WishList";
 
 export class WishListBook {
     constructor(
-        readonly id: any,
+        readonly id: string | number,
         readonly wishlist: WishList,
         readonly book: Book
     ) {}
