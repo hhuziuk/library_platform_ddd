8c64b2b6778a694a157af3c30e8b468a39c18347
hhuziuk <huziukwork@gmail.com>
2023-10-20 11:43:26 +0200

    changed types for id fields in core/repositories/TypeRepository.ts and core/repositories/dto/TypeDto.ts

--- core/repositories/TypeRepository/TypeRepository.ts
+++ core/repositories/TypeRepository/TypeRepository.ts
@@ -4,6 +4,6 @@ import {Type} from "../../domain/Type";
 export interface TypeRepository {
     createType(name: string): Promise<TypeDto>;
     getAllTypes(): Promise<Type[]>;
-    getTypeById(id: any): Promise<Type | null>;
-    deleteType(id: any): Promise<void>;
+    getTypeById(id: string | number): Promise<Type | null>;
+    deleteType(id: string | number): Promise<void>;
 }
--- core/repositories/TypeRepository/dto/TypeDto.ts
+++ core/repositories/TypeRepository/dto/TypeDto.ts
@@ -1,6 +1,6 @@
 import {Type} from "../../../domain/Type";
 export class TypeDto {
-    readonly id: any;
+    readonly id: string | number;
     readonly name: string;
 
     constructor(type: Type) {
